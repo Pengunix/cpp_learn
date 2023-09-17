d79793317bb93cfdb5371a7db0bf2b03b1732a8e
Pengunix <hoshimaemi@outlook.com>
2023-09-17 09:28:07 +0800

    添加洛谷

--- ET-Campus/差分.cpp
+++ ET-Campus/差分.cpp
@@ -3,10 +3,40 @@ using namespace std;
 using ll = long long;
 
 const int N = 1e5 + 10;
-ll a[N], prefix[N];
+ll a[N], diff[N], prefix[N];
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
   cout.tie(0);
+
+  int n;
+  cin >> n;
+
+  for (int i = 1; i <= n; ++i)
+    cin >> a[i];
+  for (int i = 1; i <= n; ++i)
+    diff[i] = a[i] - a[i - 1]; // 求差分数组
+
+  int m;
+  cin >> m;
+  while (m--) {
+    ll l, r, x;
+    cin >> l >> r >> x;
+    diff[l] += x, diff[r + 1] -= x; // 区间修改
+  }
+
+  for (int i = 1; i <= n; ++i)
+    a[i] = a[i - 1] + diff[i]; // 差分还原
+
+  for (int i = 1; i <= n; ++i)
+    prefix[i] = prefix[i - 1] + a[i];
+
+  int q;
+  cin >> q;
+  while (q--) {
+    int l, r;
+    cin >> l >> r;
+    cout << prefix[r] - prefix[l - 1] << '\n';
+  }
 }
---
+++ luogu/P8772.cpp
@@ -0,0 +1,24 @@
+// 给定n个整数，求它们两两相乘再相加的和
+#include <iostream>
+using namespace std;
+using ll = long long;
+const int N = 2 * 1e5 + 10;
+ll a[N], sum[N];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; ++i)
+    cin >> a[i];
+  for (int i = 1; i <= n; ++i)
+    sum[i] = sum[i - 1] + a[i];
+
+  ll ans = 0;
+  for (int i = 1; i <= n - 1; i++)
+    ans += a[i] * (sum[n] - sum[i]);
+  cout << ans << '\n';
+}
